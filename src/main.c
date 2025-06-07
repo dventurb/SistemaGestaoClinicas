0ef58587d1088d250c612a6814027055859beb1b
dventurb <dventura@protonmail.com>
2025-06-07 13:46:49 +0100

    Incorporate graphical interface

---
+++ src/main.c
@@ -0,0 +1,24 @@
+// BIBLIOTECAS
+#include <gtk/gtk.h>
+#include "mainWindow.h"
+#include "menu.h"
+
+// MAIN 
+int main(int argc, int **argv){
+  GtkApplication *app;
+    
+  ST_CLIENTE clientes[MAX_CLIENTES];
+  ST_MEDICO medicos[MAX_MEDICOS];
+  ST_CONSULTA consultas[MAX_CONSULTAS];
+    
+  carregarFicheiroCliente(clientes);
+  carregarFicheiroMedico(medicos);
+  carregarFicheiroConsulta(consultas, clientes, medicos);
+    
+  app = gtk_application_new("clinica.gestao", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK(createMainWindow), NULL);
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+    
+  return status;
+}
---
+++ src/ui_utils.c
@@ -0,0 +1,17 @@
+#include "ui_utils.h"
+
+void createButtonWithImageLabel(ST_BUTTON *button, const char *pathToImage, const char *text) {
+  button->button = gtk_button_new();
+  
+  button->box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+  gtk_widget_set_margin_start(button->box, 5);
+  gtk_widget_set_margin_end(button->box, 5);
+  gtk_button_set_child(GTK_BUTTON(button->button), button->box);
+  
+  button->image = gtk_image_new_from_file(pathToImage);
+  gtk_widget_set_size_request(button->image, 30, 30);
+  gtk_box_append(GTK_BOX(button->box), button->image);
+
+  button->label = gtk_label_new(text);
+  gtk_box_append(GTK_BOX(button->box), button->label);
+}
